c2a84ab0c0ff97aa91148ac4ea8a3e0d145ca5cd
Lorenzo Ordóñez <lordman1982@gmail.com>
2019-08-01 23:50:33 +0200

    Check headers

--- main.go
+++ main.go
@@ -116,7 +116,6 @@ func loadAvailableCars(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		log.Println("There is a failure in the expected headers")
 	} else {
-		// w.Header().Set("Content Type", "application/json")
 		// Remove all previous data (existing journeys and cars)
 		cars = nil
 		journeys = nil
@@ -131,7 +130,7 @@ func loadAvailableCars(w http.ResponseWriter, r *http.Request) {
 		if decoded != nil || valid != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			cars = nil
-			log.Println("There is a failure in the request format, expected headers, or the payload can't be unmarshaled")
+			log.Println("There is a failure in the request format or the payload can't be unmarshaled")
 		} else {
 			w.WriteHeader(http.StatusOK)
 			log.Printf("%+v", cars)
@@ -149,20 +148,25 @@ func loadAvailableCars(w http.ResponseWriter, r *http.Request) {
 // 400 Bad Request When there is a failure in the request format or the
 // payload can't be unmarshaled.
 func requestJourney(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "application/json")
-	var newJourney Journey
-	// Decode JSON
-	decoded := json.NewDecoder(r.Body).Decode(&newJourney)
-	// We need to check if the data provided match the requirements
-	valid := validateJourney(newJourney)
-	if decoded != nil || valid != nil {
+	contentType := r.Header.Get("Content-type")
+	if contentType != "application/json" {
 		w.WriteHeader(http.StatusBadRequest)
-		log.Println("There is a failure in the request format or the payload can't be unmarshaled")
+		log.Println("There is a failure in the expected headers")
 	} else {
-		w.WriteHeader(http.StatusAccepted)
-		journeys = append(journeys, newJourney)
-		assignJourneysToCars()
-		log.Println("The group has been registered correctly")
+		var newJourney Journey
+		// Decode JSON
+		decoded := json.NewDecoder(r.Body).Decode(&newJourney)
+		// We need to check if the data provided match the requirements
+		valid := validateJourney(newJourney)
+		if decoded != nil || valid != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Println("There is a failure in the request format or the payload can't be unmarshaled")
+		} else {
+			w.WriteHeader(http.StatusAccepted)
+			journeys = append(journeys, newJourney)
+			assignJourneysToCars()
+			log.Println("The group has been registered correctly")
+		}
 	}
 }
 
@@ -176,55 +180,61 @@ func requestJourney(w http.ResponseWriter, r *http.Request) {
 // 400 Bad Request When there is a failure in the request format or the
 // payload can't be unmarshaled.
 func dropOff(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "application/x-www-form-urlencoded")
-	parsed := r.ParseForm()
-	if parsed != nil { // The form can not be parsed
+	contentType := r.Header.Get("Content-type")
+	if contentType != "application/x-www-form-urlencoded" {
 		w.WriteHeader(http.StatusBadRequest)
-		log.Println("The form can not be parsed")
-	} else { // The form can be parsed
-		idstring := r.FormValue("ID")
-		if idstring != "" { // The key "ID" is present in the form
-			idint, converted := strconv.Atoi(idstring)
-			if converted != nil { // The id can not be converted from string to int
-				w.WriteHeader(http.StatusBadRequest)
-				log.Println("The id can not be converted from string to int")
-			} else { // The id has been converted from string to int
-				// Find the journey
-				i := sort.Search(len(journeys), func(i int) bool { return journeys[i].ID >= idint })
-				if i < len(journeys) && journeys[i].ID == idint {
-					// Find the car assiociated to the journey
-					sort.SliceStable(cars, func(j, k int) bool {
-						return cars[j].ID < cars[k].ID
-					})
-					l := sort.Search(len(cars), func(l int) bool { return cars[l].ID >= journeys[i].CarID })
-					if l < len(cars) && cars[l].ID == journeys[i].CarID {
-						log.Printf("%+v", cars[l])
-						// Free seats in the car
-						cars[l].Available += journeys[i].People
-						// Remove the journey for the journeys slice
-						journeys = append(journeys[:i], journeys[i+1:]...)
-						// Once the journey has been removed and the car seats released
-						// we should try to assign unassigned groups
-						assignJourneysToCars()
-						w.WriteHeader(http.StatusNoContent)
-						log.Println("The group has been unregistered correctly.")
-					} else {
-						// This would mean the group has no car assigned
-						// so we would not net to release the car or
-						// try to assign unassigned groups
-						journeys = append(journeys[:i], journeys[i+1:]...)
-						w.WriteHeader(http.StatusNoContent)
-						log.Println("The group has been unregistered correctly.")
-						log.Printf("%+v", journeys)
-					}
-				} else {
-					w.WriteHeader(http.StatusNotFound)
-					log.Println("The group has not been found.")
-				}
-			}
-		} else { // The key "ID" is not present in the form
+		log.Println("There is a failure in the expected headers")
+	} else {
+		w.Header().Set("Content Type", "application/x-www-form-urlencoded")
+		parsed := r.ParseForm()
+		if parsed != nil { // The form can not be parsed
 			w.WriteHeader(http.StatusBadRequest)
-			log.Println("The key \"ID\" is not present in the form")
+			log.Println("The form can not be parsed")
+		} else { // The form can be parsed
+			idstring := r.FormValue("ID")
+			if idstring != "" { // The key "ID" is present in the form
+				idint, converted := strconv.Atoi(idstring)
+				if converted != nil { // The id can not be converted from string to int
+					w.WriteHeader(http.StatusBadRequest)
+					log.Println("The id can not be converted from string to int")
+				} else { // The id has been converted from string to int
+					// Find the journey
+					i := sort.Search(len(journeys), func(i int) bool { return journeys[i].ID >= idint })
+					if i < len(journeys) && journeys[i].ID == idint {
+						// Find the car assiociated to the journey
+						sort.SliceStable(cars, func(j, k int) bool {
+							return cars[j].ID < cars[k].ID
+						})
+						l := sort.Search(len(cars), func(l int) bool { return cars[l].ID >= journeys[i].CarID })
+						if l < len(cars) && cars[l].ID == journeys[i].CarID {
+							log.Printf("%+v", cars[l])
+							// Free seats in the car
+							cars[l].Available += journeys[i].People
+							// Remove the journey for the journeys slice
+							journeys = append(journeys[:i], journeys[i+1:]...)
+							// Once the journey has been removed and the car seats released
+							// we should try to assign unassigned groups
+							assignJourneysToCars()
+							w.WriteHeader(http.StatusNoContent)
+							log.Println("The group has been unregistered correctly.")
+						} else {
+							// This would mean the group has no car assigned
+							// so we would not net to release the car or
+							// try to assign unassigned groups
+							journeys = append(journeys[:i], journeys[i+1:]...)
+							w.WriteHeader(http.StatusNoContent)
+							log.Println("The group has been unregistered correctly.")
+							log.Printf("%+v", journeys)
+						}
+					} else {
+						w.WriteHeader(http.StatusNotFound)
+						log.Println("The group has not been found.")
+					}
+				}
+			} else { // The key "ID" is not present in the form
+				w.WriteHeader(http.StatusBadRequest)
+				log.Println("The key \"ID\" is not present in the form")
+			}
 		}
 	}
 }
@@ -243,49 +253,54 @@ func dropOff(w http.ResponseWriter, r *http.Request) {
 // 400 Bad Request When there is a failure in the request format or the
 // payload can't be unmarshaled.
 func locateCar(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Accept", "application/json")
-	parsed := r.ParseForm()
-	if parsed != nil { // The form can not be parsed
+	contentType := r.Header.Get("Content-type")
+	if contentType != "application/x-www-form-urlencoded" {
 		w.WriteHeader(http.StatusBadRequest)
-		log.Println("The form can not be parsed")
-	} else { // The form can be parsed
-		idstring := r.FormValue("ID")
-		if idstring != "" { // The key "ID" is present in the form
-			idint, converted := strconv.Atoi(idstring)
-			if converted != nil { // The id can not be converted from string to int
-				w.WriteHeader(http.StatusBadRequest)
-				log.Println("The id can not be converted from string to int")
-			} else { // The id has been converted from string to int
-				// Find the journey
-				i := sort.Search(len(journeys), func(i int) bool { return journeys[i].ID >= idint })
-				if i < len(journeys) && journeys[i].ID == idint {
-					// Check if the journey have a car assigned
-					if journeys[i].CarID == 0 {
-						w.WriteHeader(http.StatusNoContent)
-						log.Println("The group is waiting to be assigned to a car")
-					} else {
-						// Find the car assiociated to the journey
-						sort.SliceStable(cars, func(j, k int) bool {
-							return cars[j].ID < cars[k].ID
-						})
-						l := sort.Search(len(cars), func(l int) bool {
-							return cars[l].ID >= journeys[i].CarID
-						})
-						err := json.NewEncoder(w).Encode(cars[l])
-						if err != nil {
-							log.Printf("%+v", err)
-						}
-						log.Println("Car details sent")
-					}
-				} else {
-					w.WriteHeader(http.StatusNotFound)
-					log.Println("The group has not been found.")
-				}
-			}
-		} else { // The key "ID" is not present in the form
+		log.Println("There is a failure in the expected headers")
+	} else {
+		w.Header().Set("Accept", "application/json")
+		parsed := r.ParseForm()
+		if parsed != nil { // The form can not be parsed
 			w.WriteHeader(http.StatusBadRequest)
-			log.Println("The key \"ID\" is not present in the form")
+			log.Println("The form can not be parsed")
+		} else { // The form can be parsed
+			idstring := r.FormValue("ID")
+			if idstring != "" { // The key "ID" is present in the form
+				idint, converted := strconv.Atoi(idstring)
+				if converted != nil { // The id can not be converted from string to int
+					w.WriteHeader(http.StatusBadRequest)
+					log.Println("The id can not be converted from string to int")
+				} else { // The id has been converted from string to int
+					// Find the journey
+					i := sort.Search(len(journeys), func(i int) bool { return journeys[i].ID >= idint })
+					if i < len(journeys) && journeys[i].ID == idint {
+						// Check if the journey have a car assigned
+						if journeys[i].CarID == 0 {
+							w.WriteHeader(http.StatusNoContent)
+							log.Println("The group is waiting to be assigned to a car")
+						} else {
+							// Find the car assiociated to the journey
+							sort.SliceStable(cars, func(j, k int) bool {
+								return cars[j].ID < cars[k].ID
+							})
+							l := sort.Search(len(cars), func(l int) bool {
+								return cars[l].ID >= journeys[i].CarID
+							})
+							err := json.NewEncoder(w).Encode(cars[l])
+							if err != nil {
+								log.Printf("%+v", err)
+							}
+							log.Println("Car details sent")
+						}
+					} else {
+						w.WriteHeader(http.StatusNotFound)
+						log.Println("The group has not been found.")
+					}
+				}
+			} else { // The key "ID" is not present in the form
+				w.WriteHeader(http.StatusBadRequest)
+				log.Println("The key \"ID\" is not present in the form")
+			}
 		}
 	}
 }
